0271735ff509a216fcfe8dad047f91a596cc02c1
Ilia balashov (Ilia) <ilya.balashow1@gmail.com>
2023-07-27 23:32:24 +0300

    added sign in using data from dummyJSON

--- app/src/main/java/com/example/retrofitreview/MainActivity.kt
+++ app/src/main/java/com/example/retrofitreview/MainActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import com.example.retrofitreview.Retrofit.ProductApi
+import com.example.retrofitreview.Retrofit.AuthRequest
+import com.example.retrofitreview.Retrofit.MainApi
+import com.example.retrofitreview.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -14,12 +17,13 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tv = findViewById<TextView>(R.id.tv)
-        val b = findViewById<Button>(R.id.button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
@@ -31,13 +35,22 @@ class MainActivity : AppCompatActivity() {
         val retrofit = Retrofit.Builder()
             .baseUrl("https://dummyjson.com").client(client)
             .addConverterFactory(GsonConverterFactory.create()).build()
-        val productApi = retrofit.create(ProductApi::class.java)
+        val mainApi = retrofit.create(MainApi::class.java)
 
-        b.setOnClickListener {
+        binding.button.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
-                val product = productApi.getProductById(3)
+                val user = mainApi.auth(
+                    AuthRequest(
+                        binding.usarename.text.toString(),
+                        binding.password.text.toString()
+                    )
+                )
                 runOnUiThread {
-                    tv.text = product.title
+                    binding.apply {
+                        Picasso.get().load(user.image).into(iv)
+                        firstName.text = user.firstName
+                        lastName.text = user.lastName
+                    }
                 }
             }
         }
